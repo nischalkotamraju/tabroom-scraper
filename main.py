cc77bf5ae8a81e7dd3f9e3f911c829c641461c2b
Nischal Kotamraju <119234796+nischalkotamraju@users.noreply.github.com>
2025-03-13 21:15:08 +0000

    Fetching Tournament Signups

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from bs4 import BeautifulSoup
 from login import login, get_password
 import requests
-from scrapers import fetch_upcoming_tournaments, fetch_tournament_history, fetch_nsda_points
+from scrapers import fetch_upcoming_tournaments, fetch_tournament_history, fetch_nsda_points, fetch_tournament_signups
 import os
 
 def clear_console():
     os.system('clear' if os.name == 'posix' else 'cls')
 
+'''
 EMAIL = input("Enter your Tabroom email: ")
 PASSWORD = get_password()
+'''
+
+EMAIL = "kotamrajunischal101@gmail.com"
+PASSWORD = "Nischal101!"
 
 session = requests.Session()
 dashboard_content = login('https://www.tabroom.com/user/login/login.mhtml', EMAIL, PASSWORD, session, nsda=False)
@@ -20,6 +25,9 @@ for upcoming_tournament in fetch_upcoming_tournaments(dashboard_soup):
 
 for past_tournament in fetch_tournament_history(dashboard_soup):
     print(past_tournament)
+
+for signup in fetch_tournament_signups(dashboard_soup):
+    print(signup)
 
 nsda_content = login('https://www.tabroom.com/user/login/login.mhtml', EMAIL, PASSWORD, session, nsda=True)
 nsda_soup = BeautifulSoup(nsda_content, 'html.parser')
--- scrapers.py
+++ scrapers.py
@@ -9,7 +9,7 @@ def fetch_upcoming_tournaments(soup):
         tournament_event = row.find('a', class_='plain full marno padmore hover').text.strip()
         tournament_confirmation = row.find('span').text.strip()
         
-        tournaments.append(tournament_name + " | " + tournament_date + " | " + tournament_event + " | " + tournament_confirmation)
+        tournaments.append({'name': tournament_name, 'date': tournament_date, 'event': tournament_event, 'confirmation': tournament_confirmation})
         
         return tournaments
     
@@ -25,9 +25,7 @@ def fetch_tournament_history(soup):
                 raw_data.append(cell.text.strip())
     
     for i in range(0, len(raw_data), 5):
-        group = raw_data[i:i+4]
-        if len(group) == 4:
-            tournament_history.append(" | ".join(group))
+        tournament_history.append({'name': raw_data[i], 'date': raw_data[i + 1], 'code': raw_data[i + 2], 'event': raw_data[i + 3]})
     
     return tournament_history
 
@@ -36,10 +34,50 @@ def fetch_nsda_points(soup):
     points_stats = soup.find_all('span', class_='threefifths leftalign semibold')
     final_text = ""
     
+    data = {'degree': '', 'current_points': 0, 'to_next_degree': 0, 'last_posted': '', 'eligibility': False }
+
     for stat in points_stats:
         for div in stat.find_all('div'):
             text = div.text.strip()
             text = ' '.join(text.split())
-            final_text += text + "\n"
+            if 'Degree of' in text:
+                degree_index = text.find("Degree of ")
+                data['degree'] = text[degree_index + 10:]
+            if 'merit points' in text:
+                bar_index = text.find(" | ")
+                merit_index = text.find(" merit points")
+                next_index = text.find(" to next degree")
+                data['current_points'] = int(text[:merit_index])
+                data['to_next_degree'] = int(text[bar_index + 3:next_index])
+            if 'Last points posted' in text:
+                last_index = text.find("posted on ")
+                data['last_posted'] = text[last_index + 10:]
+            if 'to enter the' in text:
+                if "You ARE eligible" in text:
+                    data['eligibility'] = True
+                else:
+                    data['elibibility'] = False
     
-    return final_text
+    return data
+
+
+# TODO: FIX THE SCRAPER DATA ISSUES
+def fetch_tournament_signups(soup):
+    print("\n--------- TOURNAMENTS TO SIGN UP FOR --------- ")
+    tournament_table = soup.find('table', id='signup_table')
+    signups = []
+    
+    for body in tournament_table.find_all('tbody'):
+        for tournament in body.find_all("tr"):
+            tournament_name = tournament.select_one('td', class_='nospace smallish').text.strip()
+            tournament_name = ' '.join(tournament_name.split())
+            tournament_date = tournament.find('td', class_='smallish').text.strip()
+            tournament_date = ' '.join(tournament_date.split())
+            tournament_events = tournament.find('td', class_='nospace centeralign nospace smallish').text.strip()
+            tournament_events = ' '.join(tournament_events.split())
+            tournament_info = tournament.find('td', class_='centeralign').text.strip()
+            tournament_info = ' '.join(tournament_info.split())
+
+            signups.append({'name': tournament_name, 'date': tournament_date, 'events': tournament_events, 'info': tournament_info})
+        
+    return signups
